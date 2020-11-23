e561d846c355ef351c6f03c7915f1431a660191e
Matt <53595224+davies3matt@users.noreply.github.com>
2020-11-23 12:36:29 +0200

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#avatarimage')
+                .attr('src', e.target.result)
+                .width(200)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
---
+++ wwwroot/js/sweethelper.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function alert() {
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You will be logged out!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, log out!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Succesfully logged out.',
+                showConfirmButton: false,
+                timer: 2000
+            })
+            var millisecondsToWait = 2000;
+            setTimeout(function () {
+                // Whatever you want to do after the wait
+                //Change this to where you're hosting...for logout
+                window.location = "https://localhost:44338/";
+            }, millisecondsToWait);
+            
+        }
+    })
+}
+
+
